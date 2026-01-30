db28422f9f0e37db35b769c9cac167e4ba4d47e9
Rsantct <rsantct@gmail.com>
2026-01-30 20:03:29 +0100

    Update miscel.py

--- bin/devcontrol/modules/miscel.py
+++ bin/devcontrol/modules/miscel.py
@@ -22,15 +22,15 @@ _MY_DIR      = os.path.dirname(__file__)
 
 LOGPATH     = f'{_MY_DIR}/../devcontrol.log'
 CFGPATH     = f'{_MY_DIR}/../devcontrol.yml'
+STATUSPATH  = f'{_MY_DIR}/../.devcontrol'
 CONFIG      = {}
-
 
 def init():
     global CONFIG
-    CONFIG = read_config(check_refresh=True)
+    CONFIG = read_config()
 
 
-def read_config(check_refresh=False):
+def read_config():
 
     config = {  'devices':  { 'plugs':{}, 'wol':{} },
                 'scripts':  {}
@@ -71,30 +71,21 @@ def read_config(check_refresh=False):
 
             config["scripts"][script]["responses"] = tmp
 
-
-    # Early return
-    if not check_refresh:
-        return config
-
-
-    # Backend update interval min 3 seconds, disabled if set to 0
-
-    bui = config["refresh"]["backend_update_interval"]
-
-    if bui <= 0:
-        pass
-
-    elif bui < 3:
+    # Backend update interval 3...30 seconds, default 5.
+    tmp = config["refresh"].get("backend_update_interval", 5)
+    if tmp < 3:
         print(f'{Fmt.RED}(devcontrol) `backend_update_interval` forced to minimum 3 seconds.{Fmt.END}')
-        config["refresh"]["backend_update_interval"] = 3
+    elif tmp > 30:
+        print(f'{Fmt.RED}(devcontrol) `backend_update_interval` forced to maximun 10 seconds.{Fmt.END}')
+    config["refresh"]["backend_update_interval"] = min(max(3, tmp), 30)
 
-    # Web client refresh interval
-
-    wri = config["refresh"]["web_refresh_interval"]
-
-    if wri < 3:
-        print(f'{Fmt.RED}(devcontrol) `web_refresh_interval` forced to minimum 3 seconds.{Fmt.END}')
-        config["refresh"]["web_refresh_interval"] = 3
+    # Web client polling interval 3...30 seconds, default 5.
+    tmp = config["refresh"].get("web_polling_interval", 5)
+    if tmp < 3:
+        print(f'{Fmt.RED}(devcontrol) `web_polling_interval` forced to minimum 3 seconds.{Fmt.END}')
+    elif tmp > 30:
+        print(f'{Fmt.RED}(devcontrol) `web_polling_interval` forced to maximun 10 seconds.{Fmt.END}')
+    config["refresh"]["web_polling_interval"] = min(max(3, tmp), 30)
 
 
     return config
@@ -125,6 +116,27 @@ def do_log(cmd, res):
         f.write(f'{strftime("%Y/%m/%d %H:%M:%S")}; {cmd}; {res}\n')
 
 
+def read_status():
+
+    st = {}
+
+    tries = 3
+    while tries:
+
+        try:
+            with open(STATUSPATH, 'r') as f:
+                st = json.loads( f.read() )
+            break
+        except:
+            tries -= 1
+            sleep(.1)
+
+    if not tries:
+        print(f'{Fmt.BOLD}(miscel.read_status) ERROR{Fmt.END}')
+
+    return st
+
+
 def dump_status():
 
     wol_keys     = CONFIG["devices"]["wol"].keys()
@@ -132,17 +144,15 @@ def dump_status():
     scripts_keys = CONFIG["scripts"].keys()
     zigbees_keys = CONFIG["zigbees"].keys()
 
-    st = _STATUS_VOID
+    st = { 'wol': {}, 'plugs': {}, 'scripts': {}, 'zigbees': {} }
 
     for wol_id in wol_keys:
         ans = wol.manage_wol( {"target": wol_id, "command": "ping"} )
         st["wol"][wol_id] = ans
 
-
     for plug_id in plug_keys:
         ans = plugs.manage_plug( {"target": plug_id, "command": "status"} )
         st["plugs"][plug_id] = ans
-
 
     for script_id in scripts_keys:
         ans = scripts.manage_script( {"target": script_id, "command": "status"} )
@@ -152,20 +162,18 @@ def dump_status():
         ans = zigbees.manage_zigbee( {"target": z_id, "command": "status"} )
         st["zigbees"][z_id] = ans
 
-
     tries = 3
-
     while tries:
 
         try:
             with open(STATUSPATH, 'w') as f:
                 f.write( json.dumps(st) )
             break
-
-        except Exception as e:
-            print(f'(miscel.dump_status) ERROR: {str(e)}')
+        except:
             tries -= 1
             sleep(.2)
 
+    if not tries:
+        print(f'{Fmt.BOLD}(miscel.dump_status) ERROR{Fmt.END}')
 
 init()
