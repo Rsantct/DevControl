cb9f8f2606cec3139bde286fba32928f70ce9f15
Rsantct <rsantct@gmail.com>
2026-02-06 16:16:40 +0100

    Update and rename bin/zigbee_mod/zigbee.py to bin/devcontrol/modules/devices_mod/zigbee.py

--- bin/devcontrol/modules/devices_mod/zigbee.py
+++ bin/devcontrol/modules/devices_mod/zigbee.py
@@ -22,7 +22,7 @@ TOPIC_DEVICES = "zigbee2mqtt/bridge/devices"
 DEVICES       = {}
 # Grupos
 TOPIC_GROUPS  = "zigbee2mqtt/bridge/groups"
-GRUPOS        = {}
+GROUPS        = {}
 
 client          = mqtt.Client()
 ZNAME           = ''
@@ -30,8 +30,19 @@ verbose         = False
 estado          = {}
 
 
+def _init():
+    actualizar_devices_y_grupos()
+
+
 def clamp(n, minn=0, maxn=100):
     return max(minn, min(n, maxn))
+
+
+def is_group(zid):
+    for grupo in GROUPS:
+        if zid == grupo.get('friendly_name'):
+            return True
+    return False
 
 
 def prepare_topics(zname=''):
@@ -56,7 +67,7 @@ def get_miembros_de_grupo(gname=''):
 
     miembros = []
 
-    for grupo in GRUPOS:
+    for grupo in GROUPS:
         if grupo.get('friendly_name', '') == gname:
             for m in grupo.get('members', []):
                 miembros.append( {'ieee_address': m.get('ieee_address', '')} )
@@ -311,7 +322,7 @@ def actualizar_devices_y_grupos():
 
     def on_message(client, userdata, msg):
 
-        global GRUPOS
+        global GROUPS
         global DEVICES
 
         if msg.topic == TOPIC_DEVICES:
@@ -322,13 +333,13 @@ def actualizar_devices_y_grupos():
                 print('--- ')
 
         elif msg.topic == TOPIC_GROUPS:
-            GRUPOS = json.loads(msg.payload.decode())
+            GROUPS = json.loads(msg.payload.decode())
             if verbose:
-                print(f'--- Recibidos GRUPOS:')
-                print( json.dumps(GRUPOS, indent=2) )
+                print(f'--- Recibidos GROUPS:')
+                print( json.dumps(GROUPS, indent=2) )
                 print('--- ')
 
-        if DEVICES and GRUPOS:
+        if DEVICES and GROUPS:
             tmp_cli.disconnect()
 
 
@@ -338,6 +349,9 @@ def actualizar_devices_y_grupos():
 
     tmp_cli.connect(MQTT_HOST, MQTT_PORT, 60)
 
-    # loop_forever se detendrá al completar DEVICES y GRUPOS en on_message()
+    # loop_forever se detendrá al completar DEVICES y GROUPS en on_message()
     tmp_cli.loop_forever()
     del tmp_cli
+
+
+_init()
