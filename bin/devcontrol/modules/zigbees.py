4a1f0f9e2acaf29a914436693c9507d3b55dc35d
Rsantct <rsantct@gmail.com>
2026-02-06 16:13:53 +0100

    Update zigbees.py

--- bin/devcontrol/modules/zigbees.py
+++ bin/devcontrol/modules/zigbees.py
@@ -11,12 +11,12 @@ import os
 import sys
 UHOME = os.path.expanduser('~')
 sys.path.append(f'{UHOME}/bin/zigbee_mod')
-sys.path.append(f'{UHOME}/bin/devcontrol/modules')
+sys.path.append(f'{UHOME}/bin/devcontrol/modules/devices_mod')
 
 import json
-import miscel as mc
-from   fmt import Fmt
-import zigbee as z
+from   fmt      import Fmt
+import miscel   as mc
+import zigbee   as z
 
 
 ZTCOLORMIN = 250
@@ -27,97 +27,67 @@ def clamp(n, minn=0, maxn=10):
     return max(minn, min(n, maxn))
 
 
-def is_group(zid):
-    for grupo in z.GRUPOS:
-        if zid == grupo.get('friendly_name'):
-            return True
-    return False
-
-
-def get_group_scenes(group):
-
-    scenes = []
-
-    group_found = next((g for g in z.GRUPOS if g['friendly_name'] == group), None)
-
-    if group_found:
-        scenes = group_found.get('scenes', [])
-
-    return scenes
-
-
-def do_command_device(device, command='state', brightness=100):
-    """ toggle not used from the web interface
-    """
-
-    result = ''
-
-    if 'sta' in command:
-        result = z.consultar_status_device(device).get('state', 'unknown')
-
-    elif command == 'on':
-        pyl = {'brightness': brightness}
-        z.enviar_mensaje(device, pyl)
-        result = 'on'
-
-    elif command == 'off':
-        pyl = {'state': 'off'}
-        z.enviar_mensaje(device, pyl)
-        result = 'off'
-
-    elif command == 'toggle':
-
-        curr = z.consultar_status_device(device).get('state')
-
-        if curr == 'on':
-            pyl = {'state': 'off'}
-            z.enviar_mensaje(device, pyl)
-            result = 'off'
-
-        elif curr == 'off':
-            pyl = {'brightness': brightness}
-            z.enviar_mensaje(device, pyl)
-            result = 'on'
-
-    return result
-
-
-def do_command_group(group, command='state', brightness=100):
+def do_command(zname, command='state', brightness=100):
     """ toggle not here, it falls under the web interface
     """
 
-    scenes = get_group_scenes(group)
+    def get_zlabel(zname):
+        for k, v in mc.CONFIG.get('devices', {}).get('zigbees', {}).items():
+            if v.get('friendly_name', '') == zname:
+                return k
+        return ''
+
+    zlabel = get_zlabel(zname)
+
+    scenes = mc.CONFIG.get('devices', {}).get('zigbees', {})[zlabel].get("scenes", [])
+
+    scenes_off = any( [s for s in scenes if s.get('name')=='off'] )
+    scenes_on  = any( [s for s in scenes if s.get('name')=='on' ] )
+
+    # We use the 1st one if any
+    if scenes_off:
+        scene_off = scenes_off[0]
+    else:
+        scene_off = {}
+    if scenes_on:
+        scene_on = scenes_on[0]
+    else:
+        scene_on = {}
 
     result = ''
 
     if 'sta' in command:
-        result = z.consultar_estado_grupo(group)
+
+        if z.is_group(zname):
+            result = z.consultar_estado_grupo(zname)
+        else:
+            result = z.consultar_status_device(zname).get('state', 'unknown')
 
     else:
 
         if command == 'on':
 
-            if  any( [s for s in scenes if s.get('id')==1] ):
+            if  scene_on:
 
-                z.enviar_mensaje(group, {'scene_recall': 1})
-                result = 'on'
-                print(f'{Fmt.BLUE}recall scene 1 for: {group}{Fmt.END}')
+                z.enviar_mensaje(zname, {'scene_recall': scene_on['id']})
+                print(f'{Fmt.BLUE}recall scene `on` for: {zname}{Fmt.END}')
 
             else:
-                z.enviar_mensaje(group, {'brightness': brightness})
-                result = 'on'
+                z.enviar_mensaje(zname, {'brightness': brightness})
+
+            result = 'on'
 
         elif command == 'off':
 
-            if  any( [s for s in scenes if s.get('id')==0] ):
+            if scene_off:
 
-                z.enviar_mensaje(group, {'scene_recall': 0})
-                result = 'off'
-                print(f'{Fmt.BLUE}recall scene 0 for: {group}{Fmt.END}')
+                z.enviar_mensaje(zname, {'scene_recall': scene_off['id']})
+                print(f'{Fmt.BLUE}recall scene `off` for: {zname}{Fmt.END}')
 
             elif command == 'off':
-                z.enviar_mensaje(group, {'state': 'off'})
-                result = 'off'
+                z.enviar_mensaje(zname, {'state': 'off'})
+
+            result = 'off'
 
     return result
 
@@ -126,18 +96,21 @@ def manage_zigbee(args):
     """
         {'target': element_name, 'command': a command phrase }
 
-        element_name as per entries under config["zigbees"]
+        element_name as per entries under config devives zigbees
 
-        result: an string (on, off, ... etc)
+        result: an string (on, off, ... or whatever descriptor)
     """
+
+    result = ''
 
     if 'target' not in args:
         return result
 
-    config = mc.read_config()
+    target    = args['target']
 
-    elem_name = args['target']
-    zname     = config.get('devices', {}).get('zigbees', {}).get(elem_name, '')
+    tmp       = mc.CONFIG.get('devices', {}).get('zigbees', {}).get(target, {})
+    zname     = tmp.get('friendly_name')
+
     tmp       = args.get('command', '').split(' ')
     command   = tmp[0] if tmp[0] else 'state'
     args      = tmp[1:]
@@ -163,16 +136,7 @@ def manage_zigbee(args):
         color_temp = int((ZTCOLORMIN + ZTCOLORMAX) / 2)
 
 
+    result = do_command(zname, command, brightness)
 
-    # Group
-    if is_group(zname):
-        result = do_command_group(zname, command, brightness)
-
-    # Individual device
-    else:
-        result = do_command_device(zname, command, brightness)
 
     return result
-
-
-z.actualizar_devices_y_grupos()
