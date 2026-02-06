a255c630d65727e0e4cec4ee41eb403b5af275e9
Rsantct <rsantct@gmail.com>
2026-02-06 19:56:36 +0100

    Update zigbees.py

--- bin/devcontrol/modules/zigbees.py
+++ bin/devcontrol/modules/zigbees.py
@@ -19,43 +19,41 @@ import miscel   as mc
 import zigbee   as z
 
 
-ZTCOLORMIN = 250
-ZTCOLORMAX = 454
+BRIGHTNESSMAX = 254
+ZTCOLORMIN    = 250
+ZTCOLORMAX    = 454
 
 
 def clamp(n, minn=0, maxn=10):
     return max(minn, min(n, maxn))
 
 
-def do_command(zname, command='state', brightness=100):
-    """ toggle not here, it falls under the web interface
-    """
+def get_scene_on_off(zlabel):
 
-    def get_zlabel(zname):
-        for k, v in mc.CONFIG.get('devices', {}).get('zigbees', {}).items():
-            if v.get('friendly_name', '') == zname:
-                return k
-        return ''
-
-    zlabel = get_zlabel(zname)
-
-    scenes = mc.CONFIG.get('devices', {}).get('zigbees', {})[zlabel].get("scenes", [])
-
-    scenes_off = any( [s for s in scenes if s.get('name')=='off'] )
-    scenes_on  = any( [s for s in scenes if s.get('name')=='on' ] )
+    scenes      = mc.CONFIG.get('devices', {}).get('zigbees', {})[zlabel].get("scenes", [])
+    scenes_off = [s for s in scenes if s.get('name')=='off']
+    scenes_on  = [s for s in scenes if s.get('name')=='on' ]
 
     # We use the 1st one if any
     if scenes_off:
         scene_off = scenes_off[0]
     else:
         scene_off = {}
+
     if scenes_on:
         scene_on = scenes_on[0]
     else:
         scene_on = {}
 
+    return scene_on, scene_off
+
+
+def do_command(zlabel, zname, command='state', brightness=0):
+    """ toggle not here, it falls under the web interface
+    """
     result = ''
 
+    # status
     if 'sta' in command:
 
         if z.is_group(zname):
@@ -63,17 +61,26 @@ def do_command(zname, command='state', brightness=100):
         else:
             result = z.consultar_status_device(zname).get('state', 'unknown')
 
+    # on / off
     else:
+
+        scene_on, scene_off = get_scene_on_off(zlabel)
 
         if command == 'on':
 
-            if  scene_on:
-
-                z.enviar_mensaje(zname, {'scene_recall': scene_on['id']})
-                print(f'{Fmt.BLUE}recall scene `on` for: {zname}{Fmt.END}')
+            if brightness:
+                z.enviar_mensaje(zname, {'brightness': brightness})
+                print(f'{Fmt.BLUE}{zname}: brightness {brightness}/{BRIGHTNESSMAX}{Fmt.END}')
 
             else:
-                z.enviar_mensaje(zname, {'brightness': brightness})
+
+                if  scene_on:
+                    z.enviar_mensaje(zname, {'scene_recall': scene_on['id']})
+                    print(f'{Fmt.BLUE}{zname}: recall scene <on>{Fmt.END}')
+
+                else:
+                    z.enviar_mensaje(zname, {'brightness': int(BRIGHTNESSMAX / 2)})
+                    print(f'{Fmt.BLUE}{zname}: brightness medium because no specied and scene <on> not found{Fmt.END}')
 
             result = 'on'
 
@@ -82,7 +89,7 @@ def do_command(zname, command='state', brightness=100):
             if scene_off:
 
                 z.enviar_mensaje(zname, {'scene_recall': scene_off['id']})
-                print(f'{Fmt.BLUE}recall scene `off` for: {zname}{Fmt.END}')
+                print(f'{Fmt.BLUE}{zname}: recall scene <off>{Fmt.END}')
 
             elif command == 'off':
                 z.enviar_mensaje(zname, {'state': 'off'})
@@ -94,11 +101,11 @@ def do_command(zname, command='state', brightness=100):
 
 def manage_zigbee(args):
     """
-        {'target': element_name, 'command': a command phrase }
+        {'target': zlabel, 'command': a command phrase }
 
-        element_name as per entries under config devives zigbees
+        zlabel must match a label entrie under config/devives/zigbees
 
-        result: an string (on, off, ... or whatever descriptor)
+        result: an string (on, off, '', ... or whatever descriptor)
     """
 
     result = ''
@@ -106,37 +113,41 @@ def manage_zigbee(args):
     if 'target' not in args:
         return result
 
-    target    = args['target']
+    zlabel    = args['target']
 
-    tmp       = mc.CONFIG.get('devices', {}).get('zigbees', {}).get(target, {})
+    # zname is the actual name under zigbee2mqtt
+    tmp       = mc.CONFIG.get('devices', {}).get('zigbees', {}).get(zlabel, {})
     zname     = tmp.get('friendly_name')
+    if not zname:
+        return 'target not found in Zigbee2MQTT'
 
     tmp       = args.get('command', '').split(' ')
     command   = tmp[0] if tmp[0] else 'state'
     args      = tmp[1:]
 
-    if not zname:
-        return 'target not found'
+    if args:
+        # Currently only brigthness as arg values
+        brightness = args[0]
+        # brightness must come in 1...10
+        try:
+            brightness = clamp( int(args[0]), 1, 10)
+        except:
+            brightness = 0
 
-    try:
-        brightness = clamp( int(args[0]), 1, 10)
-    except:
-        brightness = 10
-
-    # brightness mut be in 0...254
+    # zigbee2mqtt brightness must be in 0...254
     try:
         brightness = int(brightness / 10 * 254)
     except:
-        brightness = 254
+        brightness = 0
 
-    # color_temp must be in ZTCOLORMIN...ZTCOLORMAX
+    # STILL NOT IN USE
+    # color_temp comes in (1...10) and must be in ZTCOLORMIN...ZTCOLORMAX
     try:
-        color_temp = int(color_temp / 100 * (ZTCOLORMAX - ZTCOLORMIN) + ZTCOLORMIN)
+        color_temp = int(color_temp / 10 * (ZTCOLORMAX - ZTCOLORMIN) + ZTCOLORMIN)
     except Exception as e:
         color_temp = int((ZTCOLORMIN + ZTCOLORMAX) / 2)
 
-
-    result = do_command(zname, command, brightness)
-
+    # Do process
+    result = do_command(zlabel, zname, command, brightness)
 
     return result
