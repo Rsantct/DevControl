dcb219bba99538143fc8194beff80d35eeb8a010
Rsantct <rsantct@gmail.com>
2026-02-08 13:57:16 +0100

    Add cron tool for managing cron jobs
    Implement a cron tool for managing cron jobs with features to prettify crontab, check job existence, add new jobs, modify existing jobs, and list all jobs.

---
+++ bin/devcontrol/modules/crontool.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Rafael Sánchez
+# This file is part of 'DevControl'
+# a very simple Home Automation app.
+
+""" a cron tool
+"""
+
+import subprocess
+import re
+from   crontab import CronTab
+
+
+def write_cron_prettified(cron, simulate=False):
+    """ a replacement of cron.write() in order to prettify the crontab:
+         - Zeropaded figures for minute, and hour
+         - Comments are moved from the job line to the line above the job
+    """
+
+    lines = cron.render().splitlines()
+
+    padded_lines = []
+    for line in lines:
+
+        # Pad minute if it is a digit
+        new_line = re.sub(r'^(\d)\s', r'0\1 ', line)
+
+        # Pad hour if it is a digit
+        new_line = re.sub(r'^(\S+)\s(\d)\s', r'\1 0\2 ', new_line)
+
+        # Move comment
+        if '#' in new_line and not new_line.strip().startswith('#'):
+            i = new_line.index('#')
+            job = new_line[:i].strip()
+            cmt = new_line[i:].strip()
+            padded_lines.append(cmt)
+            padded_lines.append(job)
+
+        else:
+            padded_lines.append(new_line)
+
+
+    final_cron_str = "\n".join(padded_lines) + "\n"
+
+    if simulate:
+        print('\n---- SIMULATION ----')
+        print(final_cron_str)
+        print()
+
+    else:
+        process = subprocess.Popen(['crontab', '-'], stdin=subprocess.PIPE)
+        process.communicate(input=final_cron_str.encode())
+        #print("✅ (crontool) Crontab saved (with zero padding minute and hour)")
+
+
+def get_cron():
+    """ crontab from the current user
+    """
+    return CronTab(user=True)
+
+
+def job_exists(patterns=()):
+    """
+        Check if there is a job whose command contains ALL the strings in 'patterns'.
+
+        <patterns> can be a string or a tuple/list of strings.
+
+        NOTICE: cron.find_comment(pattern) ONLY WORKS for trailing
+                comments in the same line as the job itself,
+                so we discard this method of searching.
+    """
+
+    cron = get_cron()
+
+    # single string --> tuple so that the loop works the same
+    if isinstance(patterns, str):
+        patterns = (patterns,)
+
+    for job in cron:
+        if all(p in job.command for p in patterns):
+            return True
+
+    return False
+
+
+def add_new_job(cron, command, comment=None, schedule="0 0 * * *", simulate=False):
+    """ schedule must in standard format 'min hour day month dow'
+
+        returns a dictionary with the modified cron object and result details
+    """
+    report = ''
+    sucess = False
+
+    # schedule string must be cleaned of extra spaces:
+    schedule = " ".join(schedule.split())
+
+    # Avoid duplicates
+    if job_exists(command):
+        print("❌ (crontool.add_new_job) job command exists, NOT added")
+        report = f'ERROR: job command exists, NOT added.'
+
+    else:
+
+        try:
+            job = cron.new(command=command, comment=comment)
+            job.setall(schedule)
+            success = True
+
+        except Exception as e:
+            print(f"❌ (crontool.add_new_job) ERROR: {e}")
+            report = f'ERROR: {e}'
+
+    return {'cron': cron, 'success': success, 'report': report}
+
+
+def modify_jobs( cron, patterns=(), new_command=None, new_schedule=None, simulate=False):
+    """ Modify ALL jobs matching by command all the pattern in the given tuple
+
+        returns a dictionary with the modified cron object and result details
+    """
+
+    # schedule string must be cleaned of extra spaces:
+    new_schedule = " ".join(new_schedule.split())
+
+    success = True
+    report  = []
+
+    found = False
+    for job in cron:
+
+        if all(pattern in job.command for pattern in patterns):
+
+            found   = True
+
+            if new_command:
+                try:
+                    job.set_command(new_command)
+                except Exception as e:
+                    print(f"(crontool.modify_jobs) ERROR: {e}")
+                    report.append( f'[{new_command}] ERROR with new_command: {e}' )
+                    success = False
+
+            if new_schedule:
+
+                try:
+                    job.setall(new_schedule)
+                except Exception as e:
+                    print(f"(crontool.modify_jobs) ERROR: {e}")
+                    report.append( f'[{new_schedule}] error with new schedule: {e}' )
+                    success = False
+
+            if success:
+                report.append( f"job modified: {job.slices} {job.command}" )
+
+    return { 'cron': cron, 'jobs_found': found, 'success': success, 'report': report}
+
+
+def list_all_jobs():
+
+    def list_job(job):
+        schedule = str(job.slices)
+        comment = job.comment if job.comment else "w/o comment"
+        command = job.command
+        print(f"{schedule:<20} | {comment:<20} | {command}")
+
+
+    for job in get_cron():
+        list_job(job)
+
